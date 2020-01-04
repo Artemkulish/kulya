f0a6a2efd153615f40b26890ae3fd86c9df85afd
root <root@instance-1.us-central1-a.c.first-one-257817.internal>
2020-01-04 08:37:16 +0000

    Ansible

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+ access_key = "your_key"
+ secret_key = "your_key"
+ region = "eu-central-1"
+}
+resource "aws_instance" "aws" {
+ ami = "ami-0cc0a36f626a4fdf5"
+ key_name   = "${aws_key_pair.ssh_key.key_name}"
+ instance_type = "t2.micro"
+}
+resource "aws_key_pair" "ssh_key" {
+ key_name = "ssh_key"
+ public_key = "${file("/home/artemkulish123/.ssh/some.pub")}"
+}
+data "aws_security_group" "default" {
+ name = "default"
+}
+resource "aws_security_group_rule" "allow_ssh" {
+    type = "ingress"
+    from_port = "22", "80"
+    to_port = "22", "80"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = "${data.aws_security_group.default.id}"
+}
