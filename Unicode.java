b8d3bdbdd97a3304098472fcaebd2984d7d631d2
Artemkulish <57228865+Artemkulish@users.noreply.github.com>
2020-02-20 15:33:23 +0200

    Create Unicode.java

---
+++ Unicode.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Let's convert numbers to the ASCII characters!");
+		System.out.println("From: ");
+		String number1 = input.nextLine();
+		
+		System.out.println("To: ");
+		String number2 = input.nextLine();
+		
+		int first = Integer.parseInt(number1);
+		int second = Integer.parseInt(number2);
+		
+		for(int i=first; i < second; i++) {
+			System.out.printf("The ASCII" + "(%d)" + " character is: " + (char)i + ".\n", i);
+		}		
+	}
+
+}
