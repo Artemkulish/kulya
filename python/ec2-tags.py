1592ceb710b1bd4db0d8e29bf6a505f74f6e831d
Artem Kulish <artemkulish123@gmail.com>
2021-11-30 12:07:02 +0200

    test

--- python/ec2-tags.py
+++ python/ec2-tags.py
@@ -2,14 +2,23 @@
 
 import time
 import sys
+
 import boto3.ec2
 import requests
 
+def get_ec2_metadata(path, header_value, method="get", header_name="X-aws-ec2-metadata-token", aws_metadata_endpoint_base_url="http://169.254.169.254/latest"):
+    try:
+        response = requests.request(method, aws_metadata_endpoint_base_url + path, headers={header_name: header_value})
+        response.raise_for_status()
+    except requests.exceptions.RequestException as err:
+        raise SystemExit(err)
+    else:
+        result = response.text
+
+    return result
 
 ## Main. ##
 if __name__ == '__main__':
-
-        aws_metadata_endpoint_base_url = "http://169.254.169.254/latest"
 
         session = boto3.Session()
         credentials = session.get_credentials()
@@ -18,26 +27,9 @@ if __name__ == '__main__':
         access_key = credentials.access_key
         secret_key = credentials.secret_key
 
-        try:
-            r = requests.put(aws_metadata_endpoint_base_url + '/api/token', headers={'X-aws-ec2-metadata-token-ttl-seconds': '60'})
-            token = r.text
-            r.raise_for_status()
-        except requests.exceptions.HTTPError as err:
-            raise SystemExit(err)
-
-        try:
-            r = requests.get(aws_metadata_endpoint_base_url + '/meta-data/placement/region', headers={'X-aws-ec2-metadata-token': token})
-            region = r.text
-            r.raise_for_status()
-        except requests.exceptions.HTTPError as err:
-            raise SystemExit(err)
-
-        try:
-            r = requests.get(aws_metadata_endpoint_base_url + '/meta-data/instance-id', headers={'X-aws-ec2-metadata-token': token})
-            instanceId = r.text  
-            r.raise_for_status()
-        except requests.exceptions.HTTPError as err:
-            raise SystemExit(err)
+        token = get_ec2_metadata("/api/token", "60", "put", "X-aws-ec2-metadata-token-ttl-seconds")
+        region = get_ec2_metadata("/meta-data/placement/region", token)
+        instanceId = get_ec2_metadata("/meta-data/instance-id", token)
 
         elb = boto3.client("elbv2", region_name=region)
         ec2 = boto3.client("ec2", region_name=region)
